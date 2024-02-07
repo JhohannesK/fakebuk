d02dc9ca3eab9b32999d71eae91fa12da3522d7b
jhohannesK <jdomeh77@gmail.com>
2024-02-07 17:33:58 +0000

    add links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { VideoComponent } from './video/video.component';
+import { GamesComponent } from './games/games.component';
+import { GroupsComponent } from './groups/groups.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		component:HomeComponent,
+		data: { title: 'Fakebuk | Home' }
+	},
+	{
+		path: 'watch',
+		component: VideoComponent,
+		data: { title: 'Fakebuk | Watch' }
+	},
+	{
+		path: 'marketplace',
+		component: MarketplaceComponent,
+		data: { title: 'Fakebuk | Watch' }
+	},
+	{
+		path: 'groups/feed',
+		component: GroupsComponent,
+		data: { title: 'Fakebuk | Watch' }
+	},
+	{
+		path: 'gaming/play',
+		component: GamesComponent,
+		data: { title: 'Fakebuk | Watch' }
+	}
+
+];
--- src/app/components/nav-links/nav-links.component.ts
+++ src/app/components/nav-links/nav-links.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TooltipComponent } from '../tooltip/tooltip.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-links',
   standalone: true,
-  imports: [TooltipComponent],
+  imports: [TooltipComponent, RouterModule],
   templateUrl: './nav-links.component.html',
   styleUrl: './nav-links.component.css'
 })
