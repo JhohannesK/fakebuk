973abb8bd0430a64e27bf15d54236ea6f0946fd3
jhohannesK <jdomeh77@gmail.com>
2024-02-07 15:19:45 +0000

    add navbar

---
+++ src/app/components/tooltip/tooltip.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-tooltip',
+  standalone: true,
+  imports: [],
+  template: `
+     <div class="absolute origin-bottom top-16 -left-2 rounded text-white opacity-0 transition-all duration-300 group-hover:opacity-100">
+                    <div class="flex max-w-xs flex-col items-center">
+                        <div class="rounded bg-gray-900 p-2 text-xs text-center shadow-lg">{{title}}</div>
+                        <!-- <div class="clip-bottom h-2 w-4 bg-gray-900"></div> -->
+                    </div>
+                </div>
+  `,
+  styleUrl: './tooltip.component.css'
+})
+export class TooltipComponent {
+	@Input() title!: string;
+}
